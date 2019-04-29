5892eb7f4eebebee20c555e4b389649f08bfc2ff
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-29 22:28:59 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,7 +15,7 @@ class PetsController < ApplicationController
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-    binding.pry
+    @pet.save
     erb :"pets/show"
   end
 
